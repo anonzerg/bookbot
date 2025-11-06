366ebcc1e53724a1f6daa3985c0b4295a1f4901f
zerg <anonzerg@proton.me>
2025-11-07 00:51:18 +0330

    sort dict of char and char count.

--- librarian.py
+++ librarian.py
@@ -1,3 +1,29 @@
 def number_of_words(text):
     words = text.split()
     return len(words)
+
+def number_of_each_char(text):
+    d = {}
+    for c in text.lower():
+        if c in d.keys():
+            d[c] += 1
+        else:
+            d[c] = 1
+
+    return d
+
+def dict_to_list(d):
+    l = []
+    for k, v in d.items():
+        if k.isalpha():
+            l.append({"char": k, "num": v})
+
+    return l
+
+def sort_on(d):
+    return d["num"]
+
+def sort_dict(d):
+    l = dict_to_list(d)
+    l.sort(reverse=True, key=sort_on)
+    return l
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from librarian import (
     number_of_words,
+    number_of_each_char,
+    sort_dict,
 )
 
 def get_book_text(book_path):
@@ -18,8 +20,11 @@ def main() -> None:
     book_path = Path("./books/frankenstein_or_the_modern_prometheus.txt")
     book_text = get_book_text(book_path)
     num_of_words = number_of_words(book_text)
-
+    num_each_char = number_of_each_char(book_text)
+    
     logger.info(f"found {num_of_words} words in {book_path.stem}")
+    for e in sort_dict(num_each_char):
+        print(f"{e["char"]}: {e["num"]}")
 
     return None
 
