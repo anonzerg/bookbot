ef2bd0b89681b31cc1fcb970482ba4fa776684f7
zerg <anonzerg@proton.me>
2025-11-06 23:55:18 +0330

    create a separate module for book related task.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import logging
 import sys
 
+from pathlib import Path
+
+from librarian import (
+    number_of_words,
+)
+
+def get_book_text(book_path):
+    with open(book_path) as book:
+        book_text = book.read()
+
+    return book_text
 
 def main() -> None:
+    
+    book_path = Path("./books/frankenstein_or_the_modern_prometheus.txt")
+    book_text = get_book_text(book_path)
+    num_of_words = number_of_words(book_text)
+
+    logger.info(f"found {num_of_words} words in {book_path.stem}")
+
     return None
 
 
